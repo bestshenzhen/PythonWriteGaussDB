8fa75a3f138474410ddd87612f3d71f4dc0f95c9
bestshenzhen <1172279671@qq.com>
2025-01-10 21:06:40 +0800

    Update execute_sql.py

--- execute_sql.py
+++ execute_sql.py
@@ -30,6 +30,7 @@ try:
     conn.autocommit = False
     
     # 2.执行nacos数据表配置脚本
+    conn=psycopg2.connect(database="com_mysql_nacos", user="root", password="123456", host="127.0.0.1", port="8000")
     cur.execute(sql.SQL(sql_script))
 
     # 提交事务
